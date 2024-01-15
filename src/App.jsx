e7596049bd9f66879e6857779a19a539a4216948
Horidas Roy <royhoridas64@gmail.com>
2024-01-15 11:33:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+  
+  return (
+    <>
+     <h2>Welcome To React+Redux</h2>
+     <Counter/>
+    </>
+  )
+}
+
+export default App
